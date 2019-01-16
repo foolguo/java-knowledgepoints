c052e8fc64ccb9b723f53561f65288c6730c4224
郭佳玥 <1103423563@qq.com>
2019-01-16 19:50:02 +0800

    运算符

---
+++ 运算符练习/src/Test.java
@@ -0,0 +1,40 @@
+public class Test {
+    public static void main(String[] args) {
+//        int maxValue=Integer.MAX_VALUE;
+//        int minValue=Integer.MIN_VALUE;
+//        long num1=maxValue+1L;//程序从右向左执行
+//        long num2=minValue-1L;//程序从右向左执行
+//        System.out.println(num1);
+//        System.out.println(num2);
+        /**
+         * 按理说 大转小 必须要强转
+         *         但是，byte<——int 有例外
+         *         首先任何整形常量都是int型，程序从右向左执行
+         */
+//         byte num1=10;
+//        System.out.println(num1);
+//        byte num2=(byte)401;//保存低8位
+//        System.out.println(num2);
+
+        /**
+         * 任何浮点数常量都是double型的
+         */
+//        double num1=10.2;
+//        float num2=1.1F;
+//        System.out.println(num1+" "+num2);
+//        System.out.println(1.1*1.1);
+//        System.out.println(1.1f*1.1f);
+//        System.out.println(1.1f*1.1);
+//        int n1=10,n2=4;
+//        System.out.println(n1/n2);
+//        System.out.println(n1/(double)n2);
+        /**
+         * char和int可以相互转换
+         */
+//        char c='A';
+//        int num=c;
+//        System.out.println(c+" "+num);
+        int data[]=new int[10];
+
+    }
+}
